e1fba60cb8a9786b9307e6f59bb8801e578fc89f
Ashwanth1920 <ashwanth.j1920@gmail.com>
2025-03-19 15:40:07 +0530

    new harshi

--- main.tf
+++ main.tf
@@ -1,8 +1,34 @@
-provider "aws" {
-  region = "us-east-1" # Change this to your desired region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345" # Change this to a unique bucket name
-  acl    = "private"
+  bucket = "myterrajenbuck01"
+
+  tags = {
+    Name        = "MyTerraformBucket"
+    Environment = "Dev"
+  }
 }
+
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-04b4f1a9cf54c11d0"
+  instance_type = "t2.micro"
+  key_name      = "newterrajenk"  # Replace with your actual key pair
+
+  tags = {
+    Name        = "MyTerraformInstance"
+    Environment = "Dev"
+  }
+  
+}
+
+
+
